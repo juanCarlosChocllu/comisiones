531c5647860ff9e928b7510a96217ec09083df0d
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-10-29 16:51:39 -0400

    log de promociones

--- src/sucursal/util/data.ts
+++ src/sucursal/util/data.ts
@@ -1,44 +1,41 @@
 export function dataEmpresa() {
-    const data = {
-      empresa: {
-        'OPTICENTRO': [
-          'SUCRE  CENTRAL',
-          'OPTICENTRO POTOSI',
-          'OPTICENTRO TUPIZA',
-          'OPTICENTRO LA PAZ',
-          'OPTICENTRO TARIJA',
-          'OPTICENTRO YACUIBA',
-          'OPTICENTRO AZURDUY',
-          'OPTICENTRO TARIJA 2',
-          'OPTICENTRO LA PAZ 2',
-          'OPTICENTRO PARAGUAY',
-          'OPTICENTRO SANTA CRUZ'
-      
-        ],
-        'ECONOVISION': [
-          'ECONOPTICA CENTRAL',
-          'ECONOPTICA PLAZA',
-          'ECONOPTICA CAMPESINO',
-          'ECONOPTICA POTOSI',
-          'ECONOPTICA CAMARGO',
-          'ECONOPTICA MORRO',
-          'ECONOPTICA POTOSI 2',
-          'ECONOPTICA MONTEAGUDO',
-          'ECONOPTICA MEDICINA',
-          'ECONOPTICA  CAMARGO CINTI',
-          'ECONOPTICA TARIJA',
-          'ECONOPTICA SANTA CRUZ INDANA',
-          'ECONOPTICA 2 SC KOLPING',
-          'ECONOPTICA EL ALTO'
-        ],
-        'TU OPTICA': ['TU OPTICA', 'TU OPTICA 2'],
-        'OPTISERVICE S.R.L': ['OFEROPTICA'],
-        //'ZONA OPTICA': ['ZONA OPTICA 1'],
-        //'OPTILAB': ['OPTILAB', 'OPTILAB TARIJA','OPTILAB PARAGUAY'],
-  
-       
-       
-      },
-    };
-    return data;
-  }
+  const data = {
+    empresa: {
+      OPTICENTRO: [
+        'SUCRE  CENTRAL',
+        'OPTICENTRO POTOSI',
+        'OPTICENTRO TUPIZA',
+        'OPTICENTRO LA PAZ',
+        'OPTICENTRO TARIJA',
+        'OPTICENTRO YACUIBA',
+        'OPTICENTRO AZURDUY',
+        'OPTICENTRO TARIJA 2',
+        'OPTICENTRO LA PAZ 2',
+        'OPTICENTRO PARAGUAY',
+        'OPTICENTRO SANTA CRUZ',
+      ],
+      ECONOVISION: [
+        'ECONOPTICA CENTRAL',
+        'ECONOPTICA PLAZA',
+        'ECONOPTICA CAMPESINO',
+        'ECONOPTICA POTOSI',
+        'ECONOPTICA CAMARGO',
+        'ECONOPTICA MORRO',
+        'ECONOPTICA POTOSI 2',
+        'ECONOPTICA MONTEAGUDO',
+        'ECONOPTICA MEDICINA',
+        'ECONOPTICA  CAMARGO CINTI',
+        'ECONOPTICA TARIJA',
+        'ECONOPTICA SANTA CRUZ INDANA',
+        'ECONOPTICA 2 SC KOLPING',
+        'ECONOPTICA EL ALTO',
+        'ECONOPTICA VILLAZON',
+      ],
+      'TU OPTICA': ['TU OPTICA', 'TU OPTICA 2'],
+      'OPTISERVICE S.R.L': ['OFEROPTICA'],
+      //'ZONA OPTICA': ['ZONA OPTICA 1'],
+      //'OPTILAB': ['OPTILAB', 'OPTILAB TARIJA','OPTILAB PARAGUAY'],
+    },
+  };
+  return data;
+}
